d2ea6d6f397cbba2dd05b6e8081a31840d75fa77
Abhishek Sahani <abhisheksahani653@gmail.com>
2021-08-22 00:14:09 +0530

    Add app.js file

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
+var txtInput = document.querySelector("#txt-input");
+var button = document.querySelector("#btn-click");
+var outputDiv = document.querySelector("#out-div");
 
+var url = "https://api.funtranslations.com/translate/minion.json";
+
+function constructURL(text){
+  return url + "?"+"text="+text;
+}
+
+button.addEventListener("click", function handleClick(){
+
+  var text = txtInput.value;
+
+  fetch(constructURL(text))
+    .then(response => response.json())
+    .then(json => {
+      var translatedText = json.contents.translated;
+      outputDiv.innerText = translatedText;
+    })
+    .catch(error => {
+      console.log("error occured ", error);
+      alert("Something wrong with server! try again after some time")
+    })
+
+})
